9decc081798b32b0eb655fb735fb7a78110ecf7c
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-06-23 20:41:57 +0100

    work on particles animation branch

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,8 +1,60 @@
 import Button from 'react-bootstrap/Button'
+import { Particles } from 'react-tsparticles'
+
+import bug from '../assets/click-a-bug1.png'
 
 export function HeroBanner({mode, modeStyles}) {
 
     const ctaButton = modeStyles[mode].ctaButton;
+
+    const particleOptions = {
+        particles: {
+          number: {
+            value: 50,
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+          },
+          color: {
+            value: "#ffffff",
+          },
+          shape: {
+            type: "circle",
+          },
+          opacity: {
+            value: 0.5,
+          },
+          size: {
+            value: 3,
+          },
+          line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1,
+          },
+          move: {
+            enable: true,
+            speed: 6,
+            out_mode: "out",
+          },
+        },
+        interactivity: {
+          events: {
+            onhover: {
+              enable: true,
+              mode: "repulse",
+            },
+            onclick: {
+              enable: true,
+              mode: "push",
+            },
+          },
+        },
+      };
+
     return (
         <div className='heroBanner'>
             <div className='hero-container'>
@@ -14,6 +66,10 @@ export function HeroBanner({mode, modeStyles}) {
                   <a href='/#projectID'>View my work</a>
                 </Button>
             </div>
+            <Particles 
+                id='tsparticles'
+                options={particleOptions}
+            />
         </div>
     )
 }
