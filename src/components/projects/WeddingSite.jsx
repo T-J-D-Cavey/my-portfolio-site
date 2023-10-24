6f558351a520bdef09e89fa2aaae46a0fb0941ee
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-10-24 10:19:05 +0100

    re-add latest projects to test if netlify issue is fixed

---
+++ src/components/projects/WeddingSite.jsx
@@ -0,0 +1,136 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+import weddingMain from "../../assets/wedding4.png";
+import weddingCarousel from "../../assets/wedding5.png";
+
+export function WeddingSite({ mode, modeStyles }) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const ctaButton = modeStyles[mode].ctaButton;
+  const secondaryButton = modeStyles[mode].secondaryButton;
+  const backgroundColor = modeStyles[mode].projectBackgroundOne;
+  const color = modeStyles[mode].color;
+
+  return (
+    <div style={{ backgroundColor: backgroundColor, color: color }}>
+      <section className="projectSection">
+        <div className="projectTextAndImageContainer">
+          <div className="projectsTextContainer">
+            <h3>My Wedding site</h3>
+            <div>
+              <p>
+                Find dates and details of my wedding, RSVP and contribute to our
+                honeymoon fund.
+              </p>
+              <ul>
+                <li>English / Mandarin conversion</li>
+                <li>Framer Motion animations</li>
+                <li>PayPal SDK</li>
+              </ul>
+            </div>
+          </div>
+          <div className="projectPicMainContainer flex flexCenter">
+            <img
+              className="projectPicMain"
+              src={weddingMain}
+              alt="wedding site screenshot"
+            />
+          </div>
+        </div>
+        <Button
+          variant={secondaryButton}
+          onClick={handleShow}
+          className="projectButton seeDetails"
+        >
+          See details
+        </Button>
+        <Button
+          variant={ctaButton}
+          onClick={handleClose}
+          className="projectButton"
+        >
+          <a
+            href="https://timandcarmen-ourwedding2024.netlify.app/"
+            target="_blank"
+          >
+            Visit site
+          </a>
+        </Button>
+      </section>
+      {/* Modal: */}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header
+          closeButton
+          style={{ backgroundColor: backgroundColor, color: color }}
+        >
+          <Modal.Title>
+            <h2>My Wedding site</h2>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ backgroundColor: "#FAF9F6", color: "#202020" }}>
+          <div className="modalTextContainer">
+            <p>
+              This site will be shared with the friends and family of my fiance
+              and myself, with details of our English and Malaysian weddings. It
+              outlines dates, locations, plans for each day, and RSVP google
+              forms, as well as a PayPal button which allows the invited guest
+              to contribute to our honeymoon fund. This site is written in
+              English and Mandarin, and uses the i18next library to allow the
+              user to switch language at the tap of a button. The site is
+              password protectect, using local storage to ensure the guest only
+              has to submit the password correctly once. Wedding bells sound
+              effects will great your first visit, and framer motion scroll
+              animations with bootstrap modals ensure a pleasant visit.
+            </p>
+            <h4>TECHNOLOGIES USED:</h4>
+            <ul>
+              <li>TypeScript / React</li>
+              <li>Vite / Rollup</li>
+              <li>Redux / React Redux / Redux Toolkit</li>
+              <li>React Bootstrap</li>
+              <li>Framer Motion</li>
+              <li>i18Next</li>
+              <li>VSCode</li>
+              <li>Git and Github</li>
+              <li>Netlify</li>
+            </ul>
+          </div>
+          <div className="modalPicContainer flexBoxCenter">
+            <img
+              className="projectPicMain"
+              src={weddingCarousel}
+              alt="Wedding site screenshot"
+            />
+          </div>
+        </Modal.Body>
+        <Modal.Footer
+          style={{ backgroundColor: backgroundColor, color: color }}
+        >
+          <Button
+            variant={secondaryButton}
+            onClick={handleClose}
+            className="projectButton"
+          >
+            Close
+          </Button>
+          <Button
+            variant={ctaButton}
+            onClick={handleClose}
+            className="projectButton"
+          >
+            <a
+              href="https://timandcarmen-ourwedding2024.netlify.app/"
+              target="_blank"
+            >
+              Visit site
+            </a>
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
---
+++ src/components/projects/XChange.jsx
@@ -0,0 +1,130 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+import xChangeMain from "../../assets/xChange1.png";
+import xChangeCarousel from "../../assets/xChange3.png";
+
+export function XChange({ mode, modeStyles }) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  let ctaButton = modeStyles[mode].ctaButton;
+  let secondaryButton = modeStyles[mode].secondaryButton;
+  let backgroundColor = modeStyles[mode].projectBackgroundTwo;
+  let color = modeStyles[mode].color;
+
+  return (
+    <div style={{ backgroundColor: backgroundColor, color: color }}>
+      <section className="projectSection">
+        <div className="projectTextAndImageContainer">
+          <div className="projectsTextContainer">
+            <h3>xChange app</h3>
+            <div>
+              <p>
+                A web app that allows quick and easy conversion of the world's
+                top currencies.
+              </p>
+              <ul>
+                <li>TypeScript</li>
+                <li>FreeCurrency API</li>
+                <li>TS-Particles</li>
+              </ul>
+            </div>
+          </div>
+          <div className="projectPicMainContainer flex flexCenter">
+            <img
+              className="projectPicMain"
+              src={xChangeMain}
+              alt="xChange app screenshot"
+            />
+          </div>
+        </div>
+        <Button
+          variant={secondaryButton}
+          onClick={handleShow}
+          className="projectButton seeDetails"
+        >
+          See details
+        </Button>
+        <Button
+          variant={ctaButton}
+          onClick={handleClose}
+          className="projectButton"
+        >
+          <a href="https://xchange-cavey-app.netlify.app/" target="_blank">
+            Visit site
+          </a>
+        </Button>
+      </section>
+      {/* Modal: */}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header
+          closeButton
+          style={{ backgroundColor: backgroundColor, color: color }}
+        >
+          <Modal.Title>
+            <h2>xChange app</h2>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ backgroundColor: "#FAF9F6", color: "#202020" }}>
+          <div className="modalTextContainer">
+            <p>
+              xChange connects to the{" "}
+              <a href="https://freecurrencyapi.com/docs" target="_blank">
+                Free currency API{" "}
+              </a>
+              for daily exchange rates of the world's top currencies. Choose any
+              of the available base and target currencies and get that day's
+              conversion rate. Use one of the inputs and the app will
+              automatically create the equivilent amount of the other currency
+              based on the conversion rate. Interact with the tsparticles
+              animation. Open up a Bootstrap modal containing links to the API
+              documentation and disclaimers. Fast and Responsive for any screen
+              size.
+            </p>
+            <h4>TECHNOLOGIES USED:</h4>
+            <ul>
+              <li>React / TypeScript</li>
+              <li>Redux / React-Redux / Redux Toolkit</li>
+              <li>Vite / Rollup</li>
+              <li>React Bootstrap</li>
+              <li>TS-Particles</li>
+              <li>Git / Github</li>
+              <li>VSCode</li>
+              <li>Netlify</li>
+            </ul>
+          </div>
+          <div className="modalPicContainer flexBoxCenter">
+            <img
+              className="projectPicMain"
+              src={xChangeCarousel}
+              alt="xChange app screenshot"
+            />
+          </div>
+        </Modal.Body>
+        <Modal.Footer
+          style={{ backgroundColor: backgroundColor, color: color }}
+        >
+          <Button
+            variant={secondaryButton}
+            onClick={handleClose}
+            className="projectButton"
+          >
+            Close
+          </Button>
+          <Button
+            variant={ctaButton}
+            onClick={handleClose}
+            className="projectButton"
+          >
+            <a href="https://xchange-cavey-app.netlify.app/" target="_blank">
+              Visit site
+            </a>
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
